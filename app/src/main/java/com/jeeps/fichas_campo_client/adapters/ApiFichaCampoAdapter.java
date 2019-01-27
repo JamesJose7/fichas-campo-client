c29f5c45b796ac7b3d248b3e6675f447b2d35be8
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-26 21:47:33 -0500

    Migrated from support to androidx library

--- app/src/main/java/com/jeeps/fichas_campo_client/adapters/ApiFichaCampoAdapter.java
+++ app/src/main/java/com/jeeps/fichas_campo_client/adapters/ApiFichaCampoAdapter.java
@@ -1,19 +1,19 @@
 package com.jeeps.fichas_campo_client.controller;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.jeeps.fichas_campo_client.model.FichaCampo;
+import com.jeeps.fichas_campo_client.ports.FichaCampoDaoPort;
 import com.jeeps.fichas_campo_client.service.HttpService;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ApiFichaCampoFacade implements HttpService.HttpServiceListener {
+public class ApiFichaCampoFacade implements FichaCampoDaoPort,
+        HttpService.HttpServiceListener {
     private static final String FICHAS_CAMPO_URL = "https://fichas-geologicas-api.herokuapp.com/api/v1/fichasCampo";
 
     private ApiFichaCampoListener listener;
@@ -30,14 +30,19 @@ public class ApiFichaCampoFacade implements HttpService.HttpServiceListener {
         gson = new Gson();
     }
 
-    public void requestFichasCampo() throws Exception {
-        httpService.sendRequest(FICHAS_CAMPO_URL);
+    @Override
+    public void requestFichasCampo() {
+        try {
+            httpService.sendRequest(FICHAS_CAMPO_URL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onSuccess(String json) {
         // Transform fichas de campo into object list
-        List<FichaCampo> fichaCampoList = new ArrayList<>();
+        List<FichaCampo> fichaCampoList;
         try {
             JSONObject jsonObject = new JSONObject(json);
             JSONObject embedded = jsonObject.getJSONObject("_embedded");
