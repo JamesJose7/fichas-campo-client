e8b01d1fe7a94cbec617c5dcac7bcd2d1b80e7f9
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-26 19:05:17 -0500

    Initial commit

---
+++ app/src/main/java/com/jeeps/fichas_campo_client/FichasCampoActivity.java
@@ -0,0 +1,13 @@
+package com.jeeps.fichas_campo_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FichasCampoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fichas_campo);
+    }
+}
